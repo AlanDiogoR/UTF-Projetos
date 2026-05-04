093bc7b3f3b0bee1cf1120526f7733854fa403da
AlanDiogoR <95175918+AlanDiogoR@users.noreply.github.com>
2026-05-04 20:40:17 -0300

    Create tables for departamento, curso, and alunos

--- Database-1/v1_2804.sql
+++ Database-1/v1_2804.sql
@@ -2,17 +2,27 @@ create database if not exists es21;
 
 use es21;
 
+
 create table departamento (
     dep_codigo int primary key,
-    dep_nome varchar(90) not null,
-    dep_ramal int not null unique check (dep_ramal between 1000 and 9999)
+    dep_nome varchar(90) not null
 );
 
-create table curso (
-    cur_codigo int primary key,
-    cur_nome varchar(90) not null,
-    dep_codigo int,
-    constraint fk_curso_departamento
-        foreign key (dep_codigo)
-        references departamento(dep_codigo)
+CREATE TABLE curso (
+    cur_codigo INT PRIMARY KEY,
+    cur_nome VARCHAR(90) NOT NULL,
+    dep_codigo INT NOT NULL,
+    CONSTRAINT fk_curso_departamento FOREIGN KEY (dep_codigo)
+        REFERENCES departamento (dep_codigo)
 );
+
+CREATE TABLE alunos (
+    alu_codigo INT PRIMARY KEY,
+    alu_nome VARCHAR(100) NOT NULL,
+    alu_idade INT,
+    alu_vlr_bolsa DECIMAL(10 , 2 ),
+    cur_codigo INT,
+    CONSTRAINT fk_aluno_curso FOREIGN KEY (cur_codigo)
+        REFERENCES curso (cur_codigo)
+);
+
